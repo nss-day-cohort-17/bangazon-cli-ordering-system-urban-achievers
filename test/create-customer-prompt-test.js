7ad7ceaa2c0414195e7be97ae1b7f61627c477a1
Michael Tambornino <michael.tambornino@gmail.com>
2017-03-16 15:52:59 -0500

    customer prompt files

---
+++ test/create-customer-prompt-test.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const {assert: {isFunction, isArray}} = require('chai')
+const  = require('../lib/create-customer/create-customer-prompt.js')
+
+describe(' ', () => {
+  it('should', () => {
+
+  });
+
+
+})
