0582cb08e58158c29a9595dd103cbe839d30bdd0
Michael Tambornino <michael.tambornino@gmail.com>
2017-03-17 15:24:09 -0500

    new test files for "add-product" option

---
+++ test/add-product-prompt-test.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const {assert: {}} = require('chai')
+const addProduct = require('../lib/add-product/add-product-prompt.js')
+
+describe('addProduct', () => {
+  it('should return an object', () => {
+
+    })
+
+})
