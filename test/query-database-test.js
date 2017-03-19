f096b9e7a85cc2b68f9656e53fe7ebbdc601c4cd
Michael Tambornino <michael.tambornino@gmail.com>
2017-03-19 11:42:01 -0500

    new file and tests for database queries

---
+++ test/query-database-test.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const {assert: {isNumber, isObject, isArray}} = require('chai')
+const { getProducts, getPaymentOptions } = require('../lib/queries/query-database.js')
+
+
+describe('getProducts', () => {
+  it('should return an object', () => {
+      isObject(getProducts())
+    });
+
+  it('should be a function', () => {
+      isFunction(getProducts)
+    })
+
+
+})
