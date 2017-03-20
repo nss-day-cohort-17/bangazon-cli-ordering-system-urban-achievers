66ff1879d1e04d876eb24127031f1c49fc75b415
Michael Tambornino <michael.tambornino@gmail.com>
2017-03-20 13:55:17 -0500

    testing products prompt

--- test/add-product-prompt-test.js
+++ test/add-product-prompt-test.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const {assert: {isFunction}} = require('chai')
-const productPrompt = require('../lib/add-product/add-product-prompt.js')
+const {assert: {isFunction, isObject}} = require('chai')
+const { productPrompt, setProductsPrompt } = require('../lib/add-product/add-product-prompt.js')
 
 describe('productPrompt', () => {
   it('should be a function', () => {
