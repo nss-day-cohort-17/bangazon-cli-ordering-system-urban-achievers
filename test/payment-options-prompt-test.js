5e3d373d5ae34cc47b81952c1d6f70ef40227c51
Michael Watson <m.watson.tn@gmail.com>
2017-03-19 21:24:45 -0500

    added payment prompt, cli, insert, args, test file

---
+++ test/payment-options-prompt-test.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const {assert: {isObject, isFunction}} = require('chai');
+const getPaymentObj = require('../lib/payment-options/payment-options-prompt.js');
+
+describe('', () => {
+
+})
+// describe('getPaymentObj', () => {
+// 	it('should be a function', () => {
+// 		isFunction()
+// 	})
+// describe('mainPrompt', () => {
+//   it('should be a function', () => {
+//     isFunction(mainPrompt)
+//   });
+// })
