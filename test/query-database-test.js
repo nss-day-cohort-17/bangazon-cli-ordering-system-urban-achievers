b6486b454d5c749c3f8463430ec8462d70366d07
Michael Tambornino <michael.tambornino@gmail.com>
2017-03-20 13:55:44 -0500

    testing product queries

--- test/query-database-test.js
+++ test/query-database-test.js
@@ -2,18 +2,25 @@
 
 const {assert: {isNumber, isObject, isArray}} = require('chai')
 const { getProducts,
+        getOneProduct,
         getPaymentOptions
       } = require('../lib/queries/query-database.js')
 
 
 describe('getProducts', () => {
   it('should return an object', () => {
-      isObject(getProducts())
-    });
-
-  it('should be a function', () => {
-      isFunction(getProducts)
+      getProducts()
+      .then((prods) => {
+        isObject(prods)
+      })
     })
+})
 
-
+describe('getOneProduct', () => {
+  it('should return an object', () => {
+      getOneProduct()
+      .then((prod) => {
+        isObject(prod)
+      })
+    })
 })
