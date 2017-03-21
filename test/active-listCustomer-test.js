6714d6d18795b776071d6616b95d5ed171cd9f94
sirwilliamiv <wjconnolly2@gmail.com>
2017-03-21 09:28:22 -0500

    added set timeout to welcome prompt after selection is made

---
+++ test/active-listCustomer-test.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const {assert: {isFunction, isArray, isObject, typeOf}} = require('chai')
+const listCustomer = require('../lib/choose-active-person/active-listCustomer.js')
+
+describe('choose-active-customer',()=>{
+  describe('get customer from db',()=> {
+    it('should be a function',()=>{
+      isFunction(listCustomer)
+    })
+    it('should return a promise',()=> {
+     typeof(listCustomer, 'promise',"hey")
+    })
+    it('should be a string', ()=>{
+      typeof(listCustomer, 'string')
+    })
+  })
+})
